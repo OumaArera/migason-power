04677b2ea910761824bf469b53e0ca5615e5f026
OumaArera <johnouma999@gmail.com>
2024-10-08 17:41:02 +0300

    feat: mzima website

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,6 +9,10 @@ const Header = () => {
     setIsMenuOpen(prevState => !prevState); // Toggle menu state
   };
 
+  const handleLinkClick = () => {
+    setIsMenuOpen(false); // Close menu on link click
+  };
+
   return (
     <header className="bg-gradient-to-r from-white to-gray-100 text-yellow-500 p-6 fixed top-0 w-full z-50 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
@@ -20,9 +24,11 @@ const Header = () => {
         {/* Toggle Button for Mobile Devices */}
         <button 
           onClick={toggleMenu}
-          className="md:hidden text-xl text-gray-900"
+          className={`md:hidden text-gray-900 focus:outline-none ${isMenuOpen ? 'transform rotate-45' : ''}`}
         >
-          {isMenuOpen ? 'Close' : 'Menu'}
+          <div className={`h-1 w-8 bg-gray-900 transition-transform duration-300 ${isMenuOpen ? 'rotate-45' : ''} mb-1`}></div>
+          <div className={`h-1 w-8 bg-gray-900 transition-opacity duration-300 ${isMenuOpen ? 'opacity-0' : 'opacity-100'}`}></div>
+          <div className={`h-1 w-8 bg-gray-900 transition-transform duration-300 ${isMenuOpen ? '-rotate-45' : ''} mt-1`}></div>
         </button>
 
         {/* Navigation Links */}
@@ -31,6 +37,7 @@ const Header = () => {
             <li>
               <Link 
                 to="/" 
+                onClick={handleLinkClick}
                 className={`${
                   location.pathname === '/' ? 'text-[#228B22]' : 'text-gray-900 hover:text-green-700'
                 } transition-colors duration-300 hover:underline p-4`}
@@ -41,6 +48,7 @@ const Header = () => {
             <li>
               <Link 
                 to="/about" 
+                onClick={handleLinkClick}
                 className={`${
                   location.pathname === '/about' ? 'text-[#228B22]' : 'text-gray-900 hover:text-green-700'
                 } transition-colors duration-300 hover:underline p-4`}
@@ -51,6 +59,7 @@ const Header = () => {
             <li>
               <Link 
                 to="/services" 
+                onClick={handleLinkClick}
                 className={`${
                   location.pathname === '/services' ? 'text-[#228B22]' : 'text-gray-900 hover:text-green-700'
                 } transition-colors duration-300 hover:underline p-4`}
@@ -61,6 +70,7 @@ const Header = () => {
             <li>
               <Link 
                 to="/join" 
+                onClick={handleLinkClick}
                 className={`${
                   location.pathname === '/join' ? 'text-[#228B22]' : 'text-gray-900 hover:text-green-700'
                 } transition-colors duration-300 hover:underline p-4`}
